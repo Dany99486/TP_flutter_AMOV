a54cf29f189aefc61e4e4bdd388a37c2994039b5
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2023-12-21 19:10:08 +0000

    Details page created!

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 import 'models/Location.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void initFirebase() async {
   await Firebase.initializeApp(
@@ -28,20 +29,24 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF02458A)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Locations'),
+      initialRoute: LocationPage.routeName,
+      routes: {
+        LocationPage.routeName : (context) => const LocationPage(title: 'Locations'),
+        LocationDetailPage.routeName: (context) =>  LocationDetailPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class LocationPage extends StatefulWidget {
+  const LocationPage({super.key, required this.title});
   final String title;
-
+  static const String routeName = '/';
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LocationPage> createState() => _LocationPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _LocationPageState extends State<LocationPage> {
   late Stream<List<Location>> locationsStream;
   bool orderByDistance = false;
   bool orderByAlphabetic = false;
@@ -159,7 +164,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
 
-                    return Center(child: CircularProgressIndicator());
+                    return const Center(child: CircularProgressIndicator());
 
                   } else if (snapshot.hasError) {
 
@@ -167,7 +172,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
                   } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
 
-                    return Center(child: CircularProgressIndicator());
+                    return const Center(child: CircularProgressIndicator());
 
                   } else {
                     return Expanded(
@@ -176,7 +181,33 @@ class _MyHomePageState extends State<MyHomePage> {
                         itemBuilder: (context, index) {
                           return Card(
                             child: ListTile(
-                              title: Text(snapshot.data![index].name),
+                              title: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(snapshot.data![index].name),
+                                  ),
+                                  Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        '${snapshot.data![index].grade}',
+                                        style: TextStyle(fontWeight: FontWeight.bold),
+                                      ),
+                                      Icon(Icons.star, color: Colors.amber),
+                                    ],
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.more_vert),
+                                    onPressed: () {
+                                      Navigator.pushNamed(
+                                        context,
+                                        LocationDetailPage.routeName,
+                                        arguments: snapshot.data![index]
+                                        );
+                                    },
+                                  ),
+                                ],
+                              ),
                             ),
                           );
                         },
@@ -188,6 +219,169 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
         ),
+    );
+  }
+}
+class LocationDetailPage extends StatelessWidget {
+  static const String routeName = '/locationDetail';
+
+  @override
+  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as Location;
+    final storage = FirebaseStorage.instance;
+    Reference ref = storage.ref().child(args.photoUrl!);
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              '${args.grade}',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Icon(Icons.star, color: Colors.amber),
+            Text(
+              args.name,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          FutureBuilder<String>(
+            future: ref.getDownloadURL(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error loading image'));
+              } else if (snapshot.hasData) {
+                return Container(
+                  height: 200, // Defina a altura da imagem
+                  width: double.infinity,
+                  child: Image.network(
+                    snapshot.data!,
+                    fit: BoxFit.cover,
+                  ),
+                );
+              } else {
+                return SizedBox();
+              }
+            },
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                InkWell(
+                  onTap: () {
+                    // Lógica para abrir o mapa
+                  },
+                  borderRadius: BorderRadius.circular(20),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF02458A),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.all(15),
+                    child: Icon(
+                      Icons.info,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 7),
+                InkWell(
+                  onTap: () {
+                    // Lógica para exibir pontos de interesse
+                  },
+                  borderRadius: BorderRadius.circular(20),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF02458A),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.all(15),
+                    child: Icon(
+                      Icons.location_on_outlined,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Evaluate Location',
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20), // Ajusta o preenchimento interno
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    // Lógica para submeter a avaliação
+                  },
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: ListView(
+                padding: EdgeInsets.zero, // Remova o padding do ListView
+                children: [
+                  SizedBox(height: 20), // Espaçamento do topo
+                  const Text(
+                    'Description:',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    args.description!,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.only(top: 10),
+        color: Colors.white, // Cor de fundo do footer
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Created by: ${args.createdBy!}",
+              style:const TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import cloud_firestore
 import firebase_core
+import firebase_storage
 import location
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
